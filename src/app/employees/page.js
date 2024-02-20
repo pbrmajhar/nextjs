0ee6bbae5c766c00ae29157a5315a06d253f621e
Md. Majharul Islam <pbrmajhar@gmail.com>
2024-02-20 17:04:04 +0600

    hello

---
+++ src/app/employees/page.js
@@ -0,0 +1,7 @@
+export default function Employees() {
+  return (
+    <div>
+      <h1 className="text-2xl text-center">Employees</h1>
+    </div>
+  );
+}
---
+++ src/app/login/page.js
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div>
+      <h1 className="text-2xl text-center">Login</h1>
+    </div>
+  );
+}
